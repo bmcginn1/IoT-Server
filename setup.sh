41cebac5b258620e86f3da239d357125c8e32396
Brian McGinnis <brian@nobox.io>
2018-03-21 14:43:37 -0500

    setupo

--- setup.sh
+++ setup.sh
@@ -1,4 +1,4 @@
 
-sudo cp -rf resources /var/www/html/resources
+sudo cp -r -f resources /var/www/html/resources
 sudo cp -f webpage.html /var/www/html/webpage.html
 sudo /etc/init.d/apache2 restart
