79a06bd05f2350204bdf2c4723d8fc2b2f181b07
Brian McGinnis <brian@nobox.io>
2018-03-21 16:51:32 +0000

    added alot of stuff time to update

--- resources/readData.py
+++ resources/readData.py
@@ -1,28 +1,64 @@
 import json
 import time
+from time import localtime
 import Adafruit_BBIO.GPIO as GPIO
 import Adafruit_BBIO.ADC as ADC
+import netifaces as ni
 
 GPIO.setup("P9_11", GPIO.IN)
 GPIO.setup("P9_12", GPIO.IN)
 ADC.setup()
-
+button1 = GPIO.input("P9_11")
+button2 = GPIO.input("P9_12")
+tempMin = 1000;
+tempMax = 0;
 data = {'Uptime': 0,
         'Button1': 0,
         'Button2': 0,
         'Temperature C': 0,
-        'Temperature F': 0}
+        'Temperature F': 0,
+        'IP': 'n/a',
+        'DateTime': 'n/a',
+        'LastChange': '',
+        'LastButton': '',
+        'TempMin': 0,
+        'TempMax': 0}
 
 while(True):
+        ni.ifaddresses('eth0')
+        data['IP'] = ni.ifaddresses('eth0')[ni.AF_INET][0]['addr']
+
         temp = ADC.read_raw("P9_40") / 22.75
         data['Temperature C'] = temp
         temp = temp *9 /5
         temp = temp + 32
         data['Temperature F'] = temp
-        data['Uptime'] += 1
-        data['Button1'] = GPIO.input("P9_11")
-        data['Button2'] = GPIO.input("P9_12")
+        if(data['Uptime'] % 36000 == 0): #When an hour has elapsed we re set the max/min temperatures
+            tempMax = 0
+            tempMin = 1000
+        if(temp > tempMax):
+            tempMax = temp
+            data['TempMax'] = temp
+        if(temp < tempMin):
+            tempMin = temp
+            data['TempMin'] = temp
+        data['Uptime'] += .1
+        data['DateTime'] = time.strftime("%d %b %Y %H:%M:%S", localtime())
 
+        if(button1 != GPIO.input("P9_11")):
+            data['Button1'] = GPIO.input("P9_11")
+            data['LastButton'] = 'Button1/P9_11'
+            data['LastChange'] = data['DateTime']
+
+        if(button2 != GPIO.input("P9_12")):
+            data['Button2'] = GPIO.input("P9_12")
+            data['LastButton'] = 'Button2/P9_12'
+            data['LastChange'] = data['DateTime']
+    
+#        data['Button1'] = GPIO.input("P9_11")
+#        data['Button2'] = GPIO.input("P9_12")
+        
         with open('data.json', 'w') as outfile:
             json.dump(data, outfile)
-        time.sleep(1)
+
+        time.sleep(.1)
