c80f33aea5ec3bf40e1821a02ac665dfa2bc9d01
Brian McGinnis <brian@nobox.io>
2018-03-21 14:32:08 -0500

    I love typos

--- resources/readData.py
+++ resources/readData.py
@@ -41,7 +41,7 @@ while(True):
             history['Hours']= timePassed.seconds
             history[timePassed.seconds] = {'Min': ('%.2f' % tempMin), 'Max': ('%.2f' % tempMax)}
             with open('/var/www/html/resources/tempHistory.json', 'w') as outfile:
-                juson.dump(history, outfile)
+                json.dump(history, outfile)
             tempMax = 0
             tempMin = 1000
 
